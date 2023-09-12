048fc75df7429af872dee3456b24b745539b1ea9
teresa881016 <teresa881016@gmail.com>
2023-09-12 21:19:30 +0900

    Controller 테스트

---
+++ src/test/java/com/sparta/myselectshop/mvc/MockSpringSecurityFilter.java
@@ -0,0 +1,25 @@
+package com.sparta.myselectshop.mvc;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+
+public class MockSpringSecurityFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        SecurityContextHolder.getContext() //인증 객체를 담고 있는 컨텍스트를 담는 공간
+                .setAuthentication((Authentication) ((HttpServletRequest) req).getUserPrincipal());
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        SecurityContextHolder.clearContext();
+    }
+}
